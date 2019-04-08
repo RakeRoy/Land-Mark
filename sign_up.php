06e668b5f5bcf6a2263de4be03404f37ec5a7942
RakeRoy <33803249+RakeRoy@users.noreply.github.com>
2019-04-08 11:56:19 +0500

    password and repeat password check
    Added the check on password and repeat password

--- sign_up.php
+++ sign_up.php
@@ -18,6 +18,10 @@ $password=$_POST['psw'];
 $r_password=$_POST['psw-repeat'];
 $city=$_POST['other_city'];
 $address=$_POST['address'];
-
-		$INSERT ="INSERT INTO `register`(`username`, `email`, `password`, `r_password`, `city`, `address`) VALUES ('$username','$email','$password','$r_password','$city','$address')";
-	mysqli_query($conn,$INSERT);
+	if($password==$r_password)
+		{$INSERT ="INSERT INTO `register`(`username`, `email`, `password`, `r_password`, `city`, `address`) VALUES ('$username','$email','$password','$r_password','$city','$address')";
+	mysqli_query($conn,$INSERT);}
+	else
+	{
+		echo"<script>alert("Password does not match")</script>";
+	}
