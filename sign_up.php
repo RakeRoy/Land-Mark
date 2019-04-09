07c68e3514fd057cb86162646ca62b27a6fb6771
RakeRoy <33803249+RakeRoy@users.noreply.github.com>
2019-04-09 18:36:51 +0500

    Update sign_up.php
    Made it working on Rakay's device.

--- sign_up.php
+++ sign_up.php
@@ -1,17 +1,19 @@
-<?php
+	<?php
 
-		$host="localhost";
+	$host="localhost";
 	$dbUsername="root";
 	$dbPassword="";
-	$dbname="youtube1";
+	$dbname="land-mafia";
 
-$conn=new mysqli($host,$dbUsername,$dbPassword,$dbname);
-if($conn->connect_error)
+$connection_obj = new mysqli($host, $dbUsername, $dbPassword, $dbname);
+if($connection_obj->connect_error)
 {
-	echo"Not Connecrted";
+	echo"Not Connected";
 }
-else{
-echo"Connected";}	
+else
+{
+	echo"Connected";
+}	
 $username=$_POST['name'];
 $email=$_POST['email'];
 $password=$_POST['psw'];
@@ -19,9 +21,15 @@ $r_password=$_POST['psw-repeat'];
 $city=$_POST['other_city'];
 $address=$_POST['address'];
 	if($password==$r_password)
-		{$INSERT ="INSERT INTO `register`(`username`, `email`, `password`, `r_password`, `city`, `address`) VALUES ('$username','$email','$password','$r_password','$city','$address')";
-	mysqli_query($conn,$INSERT);}
+	{
+		
+		$INSERT ="INSERT INTO `land_mafia`(`username`, `email`, `password`, `r_password`, `city`, `address`) VALUES ('$username','$email','$password','$r_password','$city','$address')";
+
+		mysqli_query($connection_obj,$INSERT);
+	}
 	else
 	{
 		echo"<script>alert('Password does not match')</script>";
 	}
+
+?>
